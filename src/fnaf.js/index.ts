086cbe3c551266f764604280d3abd95173fd4ecd
Ben Brady <benbrady04.bb@gmail.com>
2026-02-16 18:28:14 +0000

    feat: added screens

--- src/fnaf.js/index.ts
+++ src/fnaf.js/index.ts
@@ -1,4 +1,5 @@
 export { runTick } from "./tick";
 export { NIGHT_DURATION, getHour } from "./time";
 export { createNight, createNumberedNight } from "./create";
+export * from "./screens";
 export type * from "./state";
