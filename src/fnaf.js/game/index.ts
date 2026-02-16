9f37ba70f1c0a9d204bf7d797c50a78817d7235f
Ben Brady <benbrady04.bb@gmail.com>
2026-02-16 18:18:19 +0000

    feat: added golden freddy

--- src/fnaf.js/game/index.ts
+++ src/fnaf.js/game/index.ts
@@ -8,15 +8,18 @@ import { applyPowerUsage } from "./power";
 import { applyUserInput } from "./input";
 import { jumpToPowerout, jumpToVictory } from "../jumps";
 import { applyDifficultyIncreases } from "./difficulites";
+import { applyGoldenFreddyChance } from "./goldenfreddy";
 
 export const runGameTick = (state: GameplayState, input: GameplayInput, dt: number) => {
   state.time += dt;
   if (state.time > NIGHT_DURATION) jumpToVictory();
   if (state.power < 0) jumpToPowerout(state);
 
+  applyUserInput(state, input);
+
+  applyGoldenFreddyChance(state, dt);
   applyDifficultyIncreases(state, dt);
   applyPowerUsage(state, dt);
-  applyUserInput(state, input);
 
   tickBonnie(state, dt);
   tickChica(state, dt);
